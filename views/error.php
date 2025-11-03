3cbc0f3476e151e1b833a19039af699a16a503d7
Siroumi Daniel <siroumi.daniel@gmail.com>
2025-11-03 01:11:47 +0000

    Update error.php

--- views/error.php
+++ views/error.php
@@ -2,17 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Error</title>
-</head>
-<body>
-    <h1>Oops! Something went wrong.</h1>
-    <p>404 - Page not found. <a href="/">Go back to home</a>.</p>
-</body>
-</html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 Error - Page Not Found</title>
 </head>
