479a61baa0014dd0572ca172f946e6fcb40af93c
Siroumi Daniel <siroumi.daniel@gmail.com>
2025-11-03 01:13:47 +0000

    removing recaptcha for security

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // config.php - Store app-wide configurations here
 return [
     'recaptcha' => [
-        'site_key' => '6Lcn3P8rAAAAABoHJRtviR3JC8gcOJhXyNrPI_4F',  
-        'secret_key' => '6Lcn3P8rAAAAAAxnfl40715oCTM2IdVeGuoTZgXg', 
+        'site_key' => '',  
+        'secret_key' => '', 
         'score_threshold' => 0.5, 
     ],
 ];
